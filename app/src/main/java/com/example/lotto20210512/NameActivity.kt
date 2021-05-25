9959c73ee345c19feb742f2d76d10996e3457eca
Hwang-uihyuk <weihyuk39@gmail.com>
2021-05-25 09:49:02 +0900

    intents

--- app/src/main/java/com/example/lotto20210512/NameActivity.kt
+++ app/src/main/java/com/example/lotto20210512/NameActivity.kt
@@ -1,11 +1,25 @@
 package com.example.lotto20210512
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class NameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+
+        val btnGo = findViewById<Button>(R.id.btnGoName)
+
+        btnGo.setOnClickListener {
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
