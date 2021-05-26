05bb22001265ef852b58306cbe21912d8c371ee9
Hwang-uihyuk <weihyuk39@gmail.com>
2021-05-26 13:25:37 +0900

    Random number generation

--- app/src/main/java/com/example/lotto20210512/MainActivity.kt
+++ app/src/main/java/com/example/lotto20210512/MainActivity.kt
@@ -4,31 +4,61 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+import java.util.*
 
 
-        val CnsResultView = findViewById<CardView>(R.id.CnsResultView)
+fun getRandomLottoNumber () : Int{
+    return Random().nextInt(45)+1
+}
+fun getRandomLottoNumbers():MutableList<Int> {
+    val lottoNumbers = mutableListOf<Int>()
 
-        CnsResultView.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ResultActivity::class.java))
 
+        while (true) {
+            var number: Int = getRandomLottoNumber()
+            var flag_existing: Int = 0
+
+          //  for (j in 0..lottoNumbers.size) {
+           if (lottoNumbers.contains(number)) {
+            //    if(number.equals(lottoNumbers[j]))
+                    flag_existing = 1
+                    //continue;
+           }
+            if (flag_existing.equals(1))
+                continue
+            else
+                lottoNumbers.add(number)
+            if (lottoNumbers.size >= 6)
+                break;
+        }
+        return lottoNumbers
+    }
+
+
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+
+            val CnsResultView = findViewById<CardView>(R.id.CnsResultView)
+            CnsResultView.setOnClickListener {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+                startActivity(intent)
+            }
             val CnsCardView = findViewById<CardView>(R.id.CnsCardView)
-
             CnsCardView.setOnClickListener {
                 startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
-
-                val CnsNameView = findViewById<CardView>(R.id.CnsNameView)
-
-                CnsNameView.setOnClickListener {
-                    startActivity(Intent(this@MainActivity, NameActivity::class.java))
-                }
-
             }
+            val CnsNameView = findViewById<CardView>(R.id.CnsNameView)
+            CnsNameView.setOnClickListener {
+                startActivity(Intent(this@MainActivity, NameActivity::class.java))
+            }
+
         }
+
     }
-}
+
+
 
